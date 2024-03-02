be2685edc47b02c073c209de069f44abdd02c5bd
amina <amina.1111@yahoo.fr>
2024-03-02 19:01:07 +0100

    do_create

--- console.py
+++ console.py
@@ -2,12 +2,15 @@
 
 """ program that contains the entry point of the command interpreter"""
 import cmd
-
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """Class : HBNHCOMMAND"""
 
     prompt = "(hbnb) "
+    myclasses = ['BaseModel']
 
     def do_quit(self, inp):
         """handle quit input."""
@@ -25,6 +28,17 @@ class HBNBCommand(cmd.Cmd):
         """Called when an empty line is entered in response to the prompt."""
         pass
 
+    def do_create(self, args):
+        if args is None or not args:
+            print('** class name missing **')
+        elif args not in HBNBCommand.myclasses:
+            print('** class doesn\'t exist **')
+        else:
+            class_name = args
+            cls = globals()[class_name]()
+            print(cls.id)
+            storage.save()
 
+    
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
