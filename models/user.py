276fb7e392b432fed55bd4793cfa8c7b2428d773
amina <amina.1111@yahoo.fr>
2024-03-03 16:20:12 +0100

    class User that inherits from BaseModel

--- models/user.py
+++ models/user.py
@@ -4,8 +4,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """Class User"""
-    def __init__(self):
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
