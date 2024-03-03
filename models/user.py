0757215d9b41e2360c1925d3001824a25b87e7f0
sachihiroo <saif3antar@gmail.com>
2024-03-03 17:49:33 +0100

    pycodestyle fixed

--- models/user.py
+++ models/user.py
@@ -2,8 +2,10 @@
 """class User that inherits from BaseModel."""
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
     """Class User"""
+
     email = ""
     password = ""
     first_name = ""
