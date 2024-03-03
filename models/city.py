4c29970c411d706280c3883b70412fb278abc100
sachihiroo <saif3antar@gmail.com>
2024-03-03 17:04:44 +0100

    Creation of a class

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""modeul of City class"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Representation of State"""
+
+    name = ""
+    state_id = ""
