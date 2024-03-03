0c0d2ec8c0251d539a5c6d2cd955d6cb8df8d983
sachihiroo <saif3antar@gmail.com>
2024-03-03 17:33:54 +0100

    Update Storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,6 +71,11 @@ class FileStorage:
 
         from models.base_model import BaseModel
         from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
 
 
         if not isfile(self.__file_path):
